9bc83d0f9cc3023da87aa8fdf7ab78aa7d6503d7
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-08 12:02:02 +0530

    Update buggy.js

--- buggy.js
+++ buggy.js
@@ -33,6 +33,38 @@ function processData() {
 
 
 
+var unused_variable = "This will trigger a warning";
+let string1 = "double quotes"
+let string2 = 'single quotes'
+const add = (a,b) => {
+    return a+b
+}
+function calculateTotal() {
+    total = price + tax;
+    return total;
+}
+function checkAge(age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log('Debug message');
+if (userInput == null) {
+    console.log("Invalid input")
+}
+function greet(name, age, location) {
+    console.log("Hello " + name);
+}
+const   spaces    =   "inconsistent";
+function  badlySpaced( ) {
+    return      true;
+}
+function processData() {
+    return "Done";
+    console.log("This will never run");
+}
 
 
 
