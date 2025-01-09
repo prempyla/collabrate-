8abcff7556c73accd36ca479a5e46b98b54de962
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-09 12:45:46 +0530

    Update pixel_colors.py

--- pixel_colors.py
+++ pixel_colors.py
@@ -1,1 +1,15 @@
-
+# Your code here
+m,n=map(int, input().split())
+matrix=[]
+for i in range(m):
+    a=list(map(int,input().split()))
+    matrix.append(a)
+q=int(input())
+for i in range(q):
+    k=int(input())
+    add=0
+    for i in range(m):
+        for j in range(n):
+            if i+j==k:
+                add+=matrix[i][j]
+    print(add)
