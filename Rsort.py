c3575dea66a81b3af258a069d5ca30252168750c
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-11 15:33:44 +0530

    Update Rsort.py

--- Rsort.py
+++ Rsort.py
@@ -1,1 +1,9 @@
 
+def sorted_list(list_of_integers):
+    n = len(list_of_integers)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if list_of_integers[j] < list_of_integers[j+1]:  
+                list_of_integers[j], list_of_integers[j+1] = list_of_integers[j+1], list_of_integers[j]
+    
+    return list_of_integers
