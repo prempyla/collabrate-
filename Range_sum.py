6209b8780ec6351a0e9a005e977fc1a8f11d78df
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-15 23:44:06 +0530

    Update Range_sum.py

--- Range_sum.py
+++ Range_sum.py
@@ -1,1 +1,16 @@
 
+lst = list(map(int, input().split()))
+q = int(input())
+
+prefix_sum = [0] * len(lst)
+prefix_sum[0] = lst[0]
+for i in range(1, len(lst)):
+    prefix_sum[i] = prefix_sum[i - 1] + lst[i]
+
+for j in range(q):
+    L, R = map(int, input().split())
+    if L > 0:
+        result = prefix_sum[R] - prefix_sum[L - 1]
+    else:
+        result = prefix_sum[R]
+    print(result)
