4523095931228f61de5885f3315c67d130499175
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-15 23:44:57 +0530

    Update Target_sum.py

--- Target_sum.py
+++ Target_sum.py
@@ -1,1 +1,16 @@
 
+def has_pair_with_sum(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        current_sum = arr[left] + arr[right]
+
+        if current_sum == target:
+            return True
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+
+    return False
