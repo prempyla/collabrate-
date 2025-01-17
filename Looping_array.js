7a7ef3406637e347f2527aa3e9ef880a0ad4903c
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-17 23:37:43 +0530

    Update Looping_array.js

--- Looping_array.js
+++ Looping_array.js
@@ -1,1 +1,13 @@
 
+// Your code here
+for (let i=0; i<inputArr.length; i++) {
+    if (inputArr[i]>0) {
+        console.log("The number is positive.")
+    } else if (inputArr[i]<0) {
+        console.log("The number is negative.")
+    } else if (inputArr[i]===0) {
+        console.log("The number is zero.")
+    } else {
+        console.log("The element is not an integer.")
+    }
+}
