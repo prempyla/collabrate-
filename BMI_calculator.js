570a6116978293cecb751ba91b9d0e41853660b6
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-17 23:29:03 +0530

    Update BMI_calculator.js

--- BMI_calculator.js
+++ BMI_calculator.js
@@ -1,1 +1,13 @@
 
+// Your code here
+let BMI=weight/(height*height);
+console.log("Your BMI is",BMI.toFixed(2))
+if (BMI<18.5) {
+    console.log("Health Category: Underweight")
+} else if (18.5<=BMI && BMI<24.9) {
+    console.log("Health Category: Normal weight")
+} else if (25<=BMI && BMI<29.9) {
+    console.log("Health Category: Overweight")
+} else {
+    console.log("Health Category: Obesity")
+}
