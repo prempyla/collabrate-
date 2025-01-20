b3f69a8d8dacc85a32ff48229879cdc9bb60941b
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-20 19:31:17 +0530

    Update clockwise.py

--- clockwise.py
+++ clockwise.py
@@ -1,1 +1,22 @@
+n, m = map(int, input().split())
+mat=[]
+for _ in range(n):
+    lst=list(map(int, input().split()))
+    mat.append(lst)
 
+matrix=[]
+for i in range(m):
+    l=[]
+    for j in range(n):
+        l.append(0)
+    matrix.append(l)
+
+for i in range(n):
+    for j in range(m):
+        matrix[j][i]=mat[i][j]
+
+for row in matrix:
+    row.reverse()
+
+for i in matrix:
+    print(" ".join(map(str, i)))
