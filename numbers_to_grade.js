539844bcfca207c82d399f17dc58466e24e5e6ea
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-24 16:12:46 +0530

    Update numbers_to_grade.js

--- numbers_to_grade.js
+++ numbers_to_grade.js
@@ -1,1 +1,14 @@
 
+// marks : you will receive marks in this variable
+// grade : assign your calculated grade to this variable
+if (marks>90 && marks<=100) {
+    grade = "A"
+} else if (marks>=80 && marks<=90) {
+    grade = "B"
+} else if (marks>=70 && marks<80) {
+    grade = "C"
+} else if (marks>=60 && marks<70) {
+    grade = "D"
+} else if (marks<60) {
+    grade = "F"
+}
