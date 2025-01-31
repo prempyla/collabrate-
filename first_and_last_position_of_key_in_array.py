ba2d9540e72e1ea1e4039429829db375b66ece29
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-31 20:13:34 +0530

    Update first_and_last_position_of_key_in_array.py

--- first_and_last_position_of_key_in_array.py
+++ first_and_last_position_of_key_in_array.py
@@ -1,1 +1,32 @@
+def searchRange(nums, target):
+    def findFirst(nums, target):
+        left, right = 0, len(nums) - 1
+        first = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                first = mid
+                right = mid - 1  
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return first
 
+    def findLast(nums, target):
+        left, right = 0, len(nums) - 1
+        last = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                last = mid
+                left = mid + 1  
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return last
+
+    first = findFirst(nums, target)
+    last = findLast(nums, target)
+    return [first, last]
