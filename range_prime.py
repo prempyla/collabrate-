568e1b3e47592d8a98009e05bede4d4788b4d4b7
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-12 23:24:36 +0530

    Update range_prime.py

--- range_prime.py
+++ range_prime.py
@@ -1,1 +1,16 @@
+n=(10**6)+1
+isPrime=[True]*n
+isPrime[0]=isPrime[1]=False
+for i in range(2, int(n**0.5)+1):
+    if isPrime[i]==False:
+        continue
+    for j in range(i*2,n,i):
+        isPrime[j]=False
 
+ans=0
+l,r=map(int,input().split())
+for i in range(l,r+1):
+    if isPrime[i]:
+        ans+=1
+
+print(ans)
