1cf694f0e9693f754c9fbaa21548ac215305a9ce
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-12 23:26:04 +0530

    Update count_prime.py

--- count_prime.py
+++ count_prime.py
@@ -1,1 +1,19 @@
+n=(10**6)+1
+isPrime=[True]*n
+isPrime[0]=isPrime[1]=False
+for i in range(2, int(n**0.5)+1):
+    if isPrime[i]==False:
+        continue
+    for j in range(i*2,n,i):
+        isPrime[j]=False
 
+prefix=[0]*n
+count=0
+for i in range(1,n):
+    if isPrime[i]:
+        count+=1
+    prefix[i]=count
+q=int(input())
+for _ in range(q):
+    l,r=map(int,input().split())
+    print(prefix[r]-prefix[l-1])
