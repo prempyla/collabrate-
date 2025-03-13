dac35df7dd4758bda2352da3d1db502017a91f15
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-13 22:21:43 +0530

    Update target_tally(easy).py

--- target_tally(easy).py
+++ target_tally(easy).py
@@ -1,1 +1,12 @@
-
+def count_lesser_or_equal(arr,target):
+    # write your code here
+    n=len(arr)
+    prefix=[0]*n
+    if arr[0]<=target:
+        prefix[0]=1
+    for i in range(1,n):
+        if arr[i]<=target:
+            prefix[i]=prefix[i-1]+1
+        else:
+            return prefix[i-1]
+    return prefix[n-1]
