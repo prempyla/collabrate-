9e89378d7cc296fb6e5186d4ab1f911718f3e6bf
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-14 22:30:54 +0530

    Update promise_all.js

--- promise_all.js
+++ promise_all.js
@@ -1,1 +1,19 @@
-
+Promise.myAll = function (arr) {
+    // write your code here
+    return new Promise((resolve,reject) => {
+      const result = [];
+      let completed = 0;
+      for (let i = 0; i < arr.length; i++) {
+        const promise = arr[i];
+        Promise.resolve(promise)
+          .then((data) => {
+            result[i] = data;
+            completed++;
+            if (completed === arr.length) {
+              resolve(result);
+            }
+          })
+          .catch((err) => reject(err));
+      }
+    });
+  }
