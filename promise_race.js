0f3642b5a2907d79bd7e70083e1e861b5a77cce8
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-14 22:31:36 +0530

    Update promise_race.js

--- promise_race.js
+++ promise_race.js
@@ -1,1 +1,11 @@
-
+Promise.myRace = function (arr) {
+  return new Promise((resolve, reject) => {
+    if (!arr || typeof arr[Symbol.iterator] !== 'function') {
+      throw new TypeError('Promise.myRace requires an iterable input');
+    }
+    
+    for (const promise of arr) {
+      Promise.resolve(promise).then(resolve, reject);
+    }
+  });
+}
