d45de45e614346a2c14b38f55787b2678e437fd8
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-17 22:50:20 +0530

    Update help_old_man.py

--- help_old_man.py
+++ help_old_man.py
@@ -1,1 +1,10 @@
+def hanoi(n, source, target, auxiliary):
+    if n == 1:
+        print(f"{n}:{source}->{target}")
+        return
+    hanoi(n - 1, source, auxiliary, target)
+    print(f"{n}:{source}->{target}")
+    hanoi(n - 1, auxiliary, target, source)
 
+n = int(input())
+hanoi(n, 'A', 'C', 'B')
