f96a4307ccf03c20305feab871a4fc091ac8861c
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-21 02:48:45 +0530

    Update exponentiation.py

--- exponentiation.py
+++ exponentiation.py
@@ -1,1 +1,10 @@
+def mod_exponentiation(a, b, m):
+    result = 1
+    a = a % m  
+    while b > 0:
+        if b % 2:  
+            result = (result * a) % m
+        a = (a * a) % m  
+        b //= 2  
+    return result
 
