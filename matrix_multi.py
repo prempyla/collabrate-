0224ff21660c5409f2aed694744c6461ddd8c4eb
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-21 02:47:13 +0530

    Update matrix_multi.py

--- matrix_multi.py
+++ matrix_multi.py
@@ -1,1 +1,23 @@
+# Matrix multiplication
+matrix1=[]
+for i in range(3):
+    row=list(map(int, input().split()))
+    matrix1.append(row)
+# print(matrix)
+matrix2=[]
+for i in range(3):
+    row=list(map(int, input().split()))
+    matrix2.append(row)
+# print(matrix2)
+
+matrix=[[0 for _ in range(3)] for _ in range(3)]
+# print(matrix)
+
+for i in range(3):
+    for j in range(3):
+        for k in range(3):
+            matrix[i][j] += matrix1[i][k] * matrix2[k][j]
+
+for ele in matrix:
+    print(*ele)
 
