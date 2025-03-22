fcfeafc72be0583b4702ab06ee37d7bb7966c88f
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-22 12:38:41 +0530

    Update count_substring(2 pointer).py

--- count_substring(2 pointer).py
+++ count_substring(2 pointer).py
@@ -1,1 +1,18 @@
+s = input().strip()
+n = len(s)
+count = 0
 
+for center in range(n):
+    left, right = center, center
+    while left >= 0 and right < n and s[left] == s[right]:
+        count += 1
+        left -= 1
+        right += 1
+
+    left, right = center, center + 1
+    while left >= 0 and right < n and s[left] == s[right]:
+        count += 1
+        left -= 1
+        right += 1
+
+print(count)
