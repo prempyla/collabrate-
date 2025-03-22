73064c448c74d23200079e0657c46b0fe2834247
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-23 00:36:40 +0530

    Update k_pass_bubblesort.py

--- k_pass_bubblesort.py
+++ k_pass_bubblesort.py
@@ -1,1 +1,7 @@
-
+n,k=map(int,input().split())
+arr=list(map(int, input().split()))
+for i in range(k):
+    for j in range(1,n):
+        if arr[j-1]>arr[j]:
+            arr[j-1],arr[j]=arr[j],arr[j-1]
+print(*arr)
