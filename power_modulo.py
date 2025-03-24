b4a94baf773d0e962307d4bde97f92b879e617b9
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-24 15:49:19 +0530

    Update power_modulo.py

--- power_modulo.py
+++ power_modulo.py
@@ -1,1 +1,11 @@
+def mod_exp(a,b,c):
+    result = 1
+    a = a % c  
 
+    while b > 0:
+        if b % 2 == 1:
+            result = (result * a) % c
+
+        a = (a * a) % c
+        b //= 2
+    return result
