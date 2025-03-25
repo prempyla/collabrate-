16fd387cfe6dee7058defcbd96fe401b77de9446
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-25 17:30:33 +0530

    Update seek_peak(linear search).py

--- seek_peak(linear search).py
+++ seek_peak(linear search).py
@@ -1,1 +1,10 @@
-
+n,k=map(int,input().split())
+arr=list(map(int, input().split()))
+sub=[]
+for i in range(n):
+    for j in range(i,n):
+        ans = arr[i:j+1]
+        l=len(ans)
+        if l==k:
+            sub.append(sum(ans))
+print(max(sub))
