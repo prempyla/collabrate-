f7a03b9d5f8f6172fdeca0e314c02f1bc2093d2d
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-27 22:09:32 +0530

    Update max_sum_divisible.py

--- max_sum_divisible.py
+++ max_sum_divisible.py
@@ -1,1 +1,11 @@
+def maxSumDivThree(nums):  
+    dp = [0, float('-inf'), float('-inf')]  
+    
+    for num in nums:  
+        temp = dp[:]  
+        for r in temp:  
+            if r != float('-inf'):  
+                dp[(r + num) % 3] = max(dp[(r + num) % 3], r + num)  
+    
+    return dp[0]  
 
