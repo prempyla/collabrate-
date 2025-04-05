5f430c5cb04dcb57aa76f095bd4176f59d2e2f2a
Chanakya Sinde <chanakyasinde@gmail.com>
2025-04-05 22:43:04 +0530

    Update shopping_cart(oops).py

--- shopping_cart(oops).py
+++ shopping_cart(oops).py
@@ -1,1 +1,17 @@
-
+class ShoppingCart:
+  def __init__(self):
+    self.items = {}
+    
+  def add_item(self,item,cost):
+    self.items[item] = cost
+    print("Added Item Successfully")
+  
+  def remove_item(self,item):
+    if item not in self.items:
+      print("Item Not Found")
+    else:
+      del self.items[item]
+      print("Removed Item Successfully")
+    
+  def calculate_total(self):
+    print(sum(self.items.values()))
