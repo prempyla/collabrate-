d729475867bf80346cac17ce02311692509348dd
Chanakya Sinde <chanakyasinde@gmail.com>
2025-04-06 20:46:53 +0530

    Update circle_oops.py

--- circle_oops.py
+++ circle_oops.py
@@ -1,1 +1,20 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = float(radius)
+        
 
+    def area(self):
+        result = (3.14 * (self.radius*self.radius))
+        return result
+
+    def circumference(self):
+        ans = 2* 3.14 * self.radius
+        return ans
+
+
+radius=float(input())
+
+circle = Circle(radius)
+
+print(circle.area())
+print(circle.circumference())
