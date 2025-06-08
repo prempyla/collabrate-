91c8e6771727f3596aa231bea0a4e33710504c62
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-08 10:18:15 +0530

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,1 +1,10 @@
-
+def insertion_sort(seq):
+    n=len(seq)
+    for i in range(n):
+        j=i-1
+        k=seq[i]
+        while(j>=0 and seq[j]>k):
+            seq[j+1]=seq[j]
+            j-=1
+        seq[j+1]=k
+    return seq
