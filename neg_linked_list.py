0e11822cb00f10f72ebd8098780a1715c8a9d14a
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-09 12:51:11 +0530

    Update neg_linked_list.py

--- neg_linked_list.py
+++ neg_linked_list.py
@@ -1,1 +1,17 @@
-
+'''
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+'''
+def isPositive(head):
+    count=0
+    current = head
+    while current is not None:
+        if current.data < 0:
+            count+=1
+        current = current.next
+    if count%2==0:
+        return "Yes"
+    else:
+        return "No"
