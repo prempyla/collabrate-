78abaf1be9ba8002df9aae894d4de0595028a6aa
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-10 10:56:12 +0530

    Update preorder_binary.py

--- preorder_binary.py
+++ preorder_binary.py
@@ -1,1 +1,19 @@
+'''
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.left = None
+        self.right = None
+'''
 
+def preorder_traversal(root):
+    lst=[]
+    preorder(root, lst)
+    return lst
+
+def preorder(root,lst):
+    if root == None:
+        return 
+    lst.append(root.val)
+    preorder(root.left,lst)
+    preorder(root.right,lst)
