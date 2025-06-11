5d95228b519172c3da28408bdd2bb2e4a4fab9ee
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-11 08:32:00 +0530

    Update node_position.py

--- node_position.py
+++ node_position.py
@@ -1,1 +1,16 @@
+'''
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+'''
 
+def node_position(head, X):
+    current = head
+    count=1
+    while current is not None:
+        if current.value == X:
+            return count
+        current = current.next
+        count+=1
+    return -1
