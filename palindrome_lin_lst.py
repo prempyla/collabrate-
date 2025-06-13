4169703c1970a3f8067786fa7530511b3c685bad
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-13 22:29:14 +0530

    Update palindrome_lin_lst.py

--- palindrome_lin_lst.py
+++ palindrome_lin_lst.py
@@ -1,1 +1,14 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+'''
 
+def is_palindrome(node):
+    lst = []
+    current = node
+    while current:
+        lst.append(current.val)
+        current = current.next
+    return lst == lst[::-1]
