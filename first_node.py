f18a6aa13099cbb715f19ccb2cdf784d052bf0c9
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-21 20:57:33 +0530

    Update first_node.py

--- first_node.py
+++ first_node.py
@@ -1,1 +1,11 @@
+'''
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+'''
 
+def first_node(head):
+    if head is None:
+        return -1
+    return head.data
