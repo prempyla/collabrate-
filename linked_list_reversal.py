fd674ef8bc80fa4c1e1158bc7119f901387e6938
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-28 12:31:08 +0530

    Update linked_list_reversal.py

--- linked_list_reversal.py
+++ linked_list_reversal.py
@@ -1,1 +1,19 @@
+'''
+    {
+        class Node:
+            def __init__(self, data):   # data -> value stored in node
+                self.data = data
+                self.next = None
+    }
+'''
+def reverseLL(head):
+    prev = None
+    current = head
 
+    while current:
+        next_node = current.next 
+        current.next = prev      
+        prev = current        
+        current = next_node   
+
+    return prev
