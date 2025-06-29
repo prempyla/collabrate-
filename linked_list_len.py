0e09f2daeb5383a0e8b70a6fdefc47b668b7bf7d
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-29 13:25:25 +0530

    Update linked_list_len.py

--- linked_list_len.py
+++ linked_list_len.py
@@ -1,1 +1,15 @@
+'''
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+'''
 
+def count_nodes(head):
+    count = 1
+    if head is None:
+        return 0
+    while head.next is not None:
+        head = head.next
+        count+=1
+    return count
