acafd983c94441a2c60c5b03d75575912a98aef4
Chanakya Sinde <chanakyasinde@gmail.com>
2025-07-02 14:41:06 +0530

    Update array_modulus_challenge.py

--- array_modulus_challenge.py
+++ array_modulus_challenge.py
@@ -1,1 +1,14 @@
-
+n,m=map(int,input().split())
+matrix=[]
+for _ in range(n):
+    row=list(map(int,input().split()))
+    matrix.append(row)
+x=int(input())
+total=0
+for row in matrix:
+    total+=sum(row)
+if total%x==0:
+    print(1)
+else:
+    print(0)
+  
