f971632373156f30dd61f125e8fa6f7489d76aaf
Chanakya Sinde <chanakyasinde@gmail.com>
2025-07-13 21:53:32 +0530

    Update sort_k_swaps.py

--- sort_k_swaps.py
+++ sort_k_swaps.py
@@ -1,1 +1,11 @@
-
+def bubbleSort(nums, k):
+    n=len(nums)
+    count=0
+    for i in range(n):
+        for j in range(1,n):
+            if nums[j] < nums[j-1]:
+                count+=1
+                nums[j],nums[j-1]=nums[j-1],nums[j]
+    if count <= k:
+        return True
+    return False
