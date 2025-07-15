e91b9e111e00749c1df92e18359cf4db378da849
Chanakya Sinde <chanakyasinde@gmail.com>
2025-07-15 22:36:57 +0530

    Update matrix_(z).py

--- matrix_(z).py
+++ matrix_(z).py
@@ -1,1 +1,14 @@
-
+n=int(input())
+matrix=[]
+for _ in range(n):
+    row=list(map(int,input().split()))
+    matrix.append(row)
+total = 0
+for i in range(n):
+    total+=matrix[0][i]
+if n >1:
+    for i in range(n):
+        total+=matrix[n-1][i]
+for j in range(1,n-1):
+    total+=matrix[j][n-j-1]
+print(total)
