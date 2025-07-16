420b8e158c6094bfb53d5e9131c8ef4e567ab788
Chanakya Sinde <chanakyasinde@gmail.com>
2025-07-16 17:27:05 +0530

    Update tree_identical.py

--- tree_identical.py
+++ tree_identical.py
@@ -1,1 +1,14 @@
+'''
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.left = None
+        self.right = None
+'''
 
+def is_identical(root1, root2):
+    if not root1 and not root2:
+        return True
+    if not root1 or not root2:
+        return False
+    return root1.val == root2.val and is_identical(root1.left, root2.left) and is_identical(root1.right, root2.right)
