f18bb98ad10df26d8cc658fb59d51f1649a3849c
Chanakya Sinde <chanakyasinde@gmail.com>
2025-07-28 22:34:49 +0530

    Update difference_operation.py

--- difference_operation.py
+++ difference_operation.py
@@ -1,1 +1,25 @@
-
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+'''
+def calculateDifference(root):
+    if root is None:
+        return 0
+    
+    current = root
+    while current.left is not None:
+        current = current.left
+    min_val = current.data
+    
+    current = root
+    while current.right is not None:
+        current = current.right
+    max_val = current.data
+    
+    product = min_val * max_val
+    sum_val = min_val + max_val
+    
+    return product - sum_val
