38dba28b67c958c48a9b60f7a4bb0018cf7db43f
Chanakya Sinde <chanakyasinde@gmail.com>
2025-08-02 23:09:49 +0530

    Update min_cost_climb_stairs.py

--- min_cost_climb_stairs.py
+++ min_cost_climb_stairs.py
@@ -1,1 +1,16 @@
+def min_cost_to_climb(cost):
+    return f(0, cost)
 
+def f(ind, cost):
+    if ind == len(cost)-1:
+        return 0
+
+    oneJump=0
+    twoJump=0
+
+    if ind < len(cost):
+        oneJump = abs(cost[ind]) + f(ind+1, cost)
+    if ind+1 < len(cost):
+        twoJump = abs(cost[ind+1]) + f(ind+2, cost)
+
+    return min(oneJump,twoJump)
