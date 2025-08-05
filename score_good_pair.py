09cb5119c9ff9d4bc90aeff38933335028759d9f
Chanakya Sinde <chanakyasinde@gmail.com>
2025-08-05 22:41:35 +0530

    Update score_good_pair.py

--- score_good_pair.py
+++ score_good_pair.py
@@ -1,1 +1,8 @@
-
+def findScoreSum(nums):
+    n=len(nums)
+    total = 0
+    for i in range(n):
+        for j in range(i,n):
+            if i < j and nums[i]==nums[j]:
+                total+=(j-i)
+    return total
