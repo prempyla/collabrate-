8f16a905e3432969a03df4ad62cdf78942e7e204
Chanakya Sinde <chanakyasinde@gmail.com>
2025-08-16 11:01:58 +0530

    Update min_max_bst.py

--- min_max_bst.py
+++ min_max_bst.py
@@ -1,1 +1,19 @@
+'''
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.left = None
+        self.right = None
+'''
 
+def find_min_max(root):
+    lst=[]
+    inorder(root,lst)
+    return lst[0],lst[-1]
+
+def inorder(root,lst):
+    if root is None:
+        return
+    inorder(root.left,lst)
+    lst.append(root.val)
+    inorder(root.right,lst)
