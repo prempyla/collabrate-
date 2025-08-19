6f5afe1d8ab300ce5ff02eef726c9a68771adcfc
Chanakya Sinde <chanakyasinde@gmail.com>
2025-08-19 23:06:17 +0530

    Update two_sum_BST.py

--- two_sum_BST.py
+++ two_sum_BST.py
@@ -1,1 +1,30 @@
+'''
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right 
+'''    
+def inorder(root, arr):
+    if root is None:
+        return 
+    inorder(root.left, arr)
+    arr.append(root.val)
+    inorder(root.right, arr)
 
+def findTarget(root, k):
+    lst = []
+    inorder(root, lst)
+    left,right = 0, len(lst) - 1
+    while left < right:
+        mid = lst[left] + lst[right]
+ 
+        if mid == k:
+            return True
+        
+        elif mid < k:
+            left += 1
+
+        else:
+            right -= 1
+    return False
