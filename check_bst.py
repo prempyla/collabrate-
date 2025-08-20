8f07f3cf2a492f8a42c89deb16c3681da33aca4f
Chanakya Sinde <chanakyasinde@gmail.com>
2025-08-20 23:23:18 +0530

    Update check_bst.py

--- check_bst.py
+++ check_bst.py
@@ -1,1 +1,16 @@
-
+'''
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+'''
+def isBST(root):
+    def solver(node, min_, max_):
+        if node is None:
+            return True
+        left = solver(node.left, min_, node.val)
+        right = solver(node.right, node.val, max_)
+        return min_ < node.val < max_ and left and right
+    
+    return solver(root, float('-inf'), float('inf'))
