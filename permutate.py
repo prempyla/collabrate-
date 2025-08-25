e7985404aff8c2ba5e063cbd4210a80821d5c599
Chanakya Sinde <chanakyasinde@gmail.com>
2025-08-25 22:28:27 +0530

    Update permutate.py

--- permutate.py
+++ permutate.py
@@ -1,1 +1,13 @@
+def permute(nums):
+    result = []
+    def helper(start):
+        if start == len(nums):
+            result.append(nums[:])
+            return 
+        for i in range(start,len(nums)):
+            nums[i],nums[start]=nums[start],nums[i]
+            helper(start+1)
+            nums[i],nums[start]=nums[start],nums[i]
 
+    helper(0)
+    return result
