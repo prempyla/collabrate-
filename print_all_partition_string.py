5341585502269bda938e557fa72d30f32500c46e
Chanakya Sinde <chanakyasinde@gmail.com>
2025-08-28 22:16:20 +0530

    Update print_all_partition_string.py

--- print_all_partition_string.py
+++ print_all_partition_string.py
@@ -1,1 +1,12 @@
-
+def all_partitions(s):
+    results = []
+    def backtrack(start, path):
+        if start == len(s):
+            results.append(path[:])
+            return
+        for end in range(start+1, len(s)+1):
+            path.append(s[start:end])
+            backtrack(end, path)
+            path.pop()
+    backtrack(0, [])
+    return results
