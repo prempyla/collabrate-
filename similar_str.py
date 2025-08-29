c8d64894d96af120c5dac89a5cabc2d2bf9e4849
Chanakya Sinde <chanakyasinde@gmail.com>
2025-08-29 23:07:21 +0530

    Update similar_str.py

--- similar_str.py
+++ similar_str.py
@@ -1,1 +1,31 @@
+#Write your complete code here
+n=int(input())
+s1=input()
+s2=input()
+lst1=[]
+lst2=[]
+for i in range(n):
+    s = s1[i]
+    lst1.append(s)
+    t = s2[i]
+    lst2.append(t)
 
+flag=False
+for j in range(n):
+    if lst1[j]=="#" and lst2[j]=="m":
+        flag = True
+    elif lst1[j]=="m" and lst2[j]=="#":
+        flag = True
+    elif lst1[j]=="*" and lst2[j]=="n":
+        flag = True
+    elif lst1[j]=="n" and lst2[j]=="*":
+        flag = True
+    elif lst1[j]==lst2[j]:
+        flag = True
+    elif lst1[j]!=lst2[j]:
+        flag = False
+        break
+if flag:
+    print("Yes")
+else:
+    print("No")
