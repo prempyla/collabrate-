b09ffad73b649e5f952a3e8dfd6827a848c48fbb
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-02 23:12:19 +0530

    Update find_subsets.py

--- find_subsets.py
+++ find_subsets.py
@@ -1,1 +1,13 @@
+def findSubsets(nums):
+    res = []
 
+    def backtrack(start, path):
+        res.append(path[:])  
+
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+
+    backtrack(0, [])
+    return res
