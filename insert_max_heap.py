24ac7052b73d8db3ae300bcb3ea57e7c1928e947
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-04 21:58:51 +0530

    Update insert_max_heap.py

--- insert_max_heap.py
+++ insert_max_heap.py
@@ -1,1 +1,16 @@
+def insert_into_heap(heap, x):
+    heap.append(x)
+    curr = len(heap)-1
+    parent = (curr - 1)//2
 
+    while True:
+        if heap[curr] > heap[parent]:
+            heap[curr],heap[parent] = heap[parent],heap[curr]
+            curr = parent
+            parent = (curr-1)//2
+            if parent < 0:
+                break
+        else:
+            break
+    return heap
+    
