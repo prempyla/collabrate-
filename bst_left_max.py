83f16f273148fa6b589e52625081b377815707f8
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-05 23:10:05 +0530

    Update bst_left_max.py

--- bst_left_max.py
+++ bst_left_max.py
@@ -1,1 +1,18 @@
-
+'''
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+'''
+def left_subtree_max(root):
+    if root is None:
+        return -1
+    if root.left is None:
+        return -1
+    return maxi(root.left)
+    
+def maxi(root):
+    if root.right is None:
+        return root.val
+    return maxi(root.right)
