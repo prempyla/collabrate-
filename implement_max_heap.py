4d9dc9d9cc592785221da33ccbf8300b5271cfcc
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-07 22:31:27 +0530

    Update implement_max_heap.py

--- implement_max_heap.py
+++ implement_max_heap.py
@@ -1,1 +1,14 @@
+import heapq
 
+class AbsoluteMaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, val: int):
+        heapq.heappush(self.heap, (-abs(val), val))
+
+    def top(self) -> int:
+        return self.heap[0][1]
+
+    def pop(self):
+        heapq.heappop(self.heap)
