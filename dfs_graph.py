95e9bd9053ed48b7370e1b03648703d00e56fd14
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-08 21:49:10 +0530

    Update dfs_graph.py

--- dfs_graph.py
+++ dfs_graph.py
@@ -1,1 +1,23 @@
+def dfsOfGraph(n, edges):
+    adj_lst = [[] for _ in range(n)]
+    visited = [False] * n
+    order = []
 
+    for u,v in edges:
+        adj_lst[v].append(u)
+        adj_lst[u].append(v)
+
+    def DFS(node):
+        visited[node] = True
+        order.append(node)
+        for neighbor in adj_lst[node]:
+            if not visited[neighbor]:
+                DFS(neighbor)
+    
+    for item in range(n):
+        if not visited[item]:
+            DFS(item)
+
+    # DFS(0)
+
+    return order
