51db26598436dcaaa2bfb3824cda488a68931a77
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-09 22:19:45 +0530

    Update all_connected_components.py

--- all_connected_components.py
+++ all_connected_components.py
@@ -1,1 +1,25 @@
+def connectedComponents(n, edges):
+    adj = [[] for _ in range(n)]
+    visited = [False] * n
+    lst = []
 
+    for u,v in edges:
+        adj[u].append(v)
+        adj[v].append(u)
+
+    def dfs(node, lst):
+        if visited[node] == True:
+            return
+        else:
+            visited[node] = True
+            lst.append(node)
+            for neigh in adj[node]:
+                dfs(neigh, lst)
+    
+    for item in range(n):
+        if visited[item] == False:
+            new_lst = []
+            dfs(item, new_lst)
+            lst.append(new_lst)
+    
+    return lst
