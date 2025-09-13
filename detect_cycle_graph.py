eabb6ad54c75a7729045493817048ed0f6d99bf4
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-13 23:02:24 +0530

    Update detect_cycle_graph.py

--- detect_cycle_graph.py
+++ detect_cycle_graph.py
@@ -1,1 +1,24 @@
+def isCyclic(n, edges):
+    adj = [[] for _ in range(n)]
+    visited = [False] * n
+    
+    for u, v in edges:
+        adj[u].append(v)
+        adj[v].append(u)
 
+    def dfs(node, parent):
+        visited[node] = True
+        for neigh in adj[node]:
+            if not visited[neigh]:
+                if dfs(neigh, node):  
+                    return True
+            elif neigh != parent:
+                return True
+        return False
+
+    for i in range(n):
+        if not visited[i]:
+            if dfs(i, -1):
+                return True
+
+    return False
