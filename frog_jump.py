9f27828d22e0386afb5b66375f3a6643b7c2b39a
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-22 21:43:09 +0530

    Update frog_jump.py

--- frog_jump.py
+++ frog_jump.py
@@ -1,1 +1,19 @@
+import sys
+sys.setrecursionlimit(10**6)
 
+def frogJump(heights):
+    dp = {}
+    return solve(0, heights, dp)
+
+def solve(index, heights, dp):
+    if index >= len(heights)-1:
+        return 0
+    if index in dp:
+        return dp[index]
+    one_jump = abs(heights[index+1]-heights[index]) + solve(index+1, heights, dp)
+    two_jump = float("inf")
+    if index+2 < len(heights):
+        two_jump = abs(heights[index+2]-heights[index]) + solve(index+2, heights, dp)
+    dp[index] = min(one_jump, two_jump)
+    
+    return dp[index]
