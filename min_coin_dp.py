1a0c1923e2b890875b3cff565ebd400400e758d3
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-25 22:27:38 +0530

    Update min_coin_dp.py

--- min_coin_dp.py
+++ min_coin_dp.py
@@ -1,1 +1,24 @@
+def minCoins(value, coins):
+    dp = {}
+    res = solver(value, coins, dp)
+    if res == float("inf"):
+        return -1
+    return res
 
+def solver(value, coins, dp):
+    if value < 0:
+        return float("inf")
+    if value == 0:
+        return 0
+
+    if value in dp:
+        return dp[value]
+
+    my_ans = float("inf")
+
+    for coin in coins:
+        small_ans = solver(value-coin, coins, dp)
+        my_ans = min(my_ans, small_ans+1)
+
+    dp[value] = my_ans
+    return my_ans
