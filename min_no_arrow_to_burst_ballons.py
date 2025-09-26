54372db1fd74a8215b6a8c63dd8a25fc23e8813e
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-26 22:47:11 +0530

    Update min_no_arrow_to_burst_ballons.py

--- min_no_arrow_to_burst_ballons.py
+++ min_no_arrow_to_burst_ballons.py
@@ -1,1 +1,12 @@
+def findMinArrowShots(points):
+    if not points:
+        return 0
+    points.sort(key=lambda x: x[1])
 
+    count = 1
+    element = points[0][1]
+    for i in range(1,len(points)):
+        if points[i][0] > element:
+            count +=1
+            element = points[i][1]
+    return count
