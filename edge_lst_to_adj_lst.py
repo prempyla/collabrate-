56fdf2debfbc429551e49d767ad2540a8625647c
Chanakya Sinde <chanakyasinde@gmail.com>
2025-10-15 23:51:47 +0530

    Update edge_lst_to_adj_lst.py

--- edge_lst_to_adj_lst.py
+++ edge_lst_to_adj_lst.py
@@ -1,1 +1,9 @@
+def convertToAdjList(n, edgeList):
+    # write your code here
+    adj = [[]for _ in range(n)]
 
+    for u,v in edgeList:
+        adj[u].append(v)
+        adj[v].append(u)
+
+    return adj
