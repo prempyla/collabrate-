7876e1cc2dfa1f686126c9b4022cec9dbcc10016
Chanakya Sinde <chanakyasinde@gmail.com>
2025-10-16 22:27:47 +0530

    Add minDistance function for edit distance calculation
    Implement dynamic programming solution for edit distance.

--- edit_distance_dp.py
+++ edit_distance_dp.py
@@ -1,1 +1,22 @@
+def minDistance(word1, word2):
+    n=len(word1)
+    m=len(word2)
+    dp = {}
+    return solve(word1, word2, n, m, dp)
 
+def solve(word1, word2, n, m, dp):
+    if (n,m) in dp:
+        return dp[(n,m)]
+    if n == 0:
+        return m
+    if m == 0:
+        return n
+    if word1[n-1] == word2[m-1]:
+        return solve(word1, word2, n-1, m-1, dp)
+    insert = 1 + solve(word1, word2, n, m-1, dp)
+    delete = 1 + solve(word1, word2, n-1, m, dp)    
+    replace = 1 + solve(word1, word2, n-1, m-1, dp) 
+
+    dp[(n,m)] = min(insert, delete, replace)
+    
+    return dp[(n,m)]
