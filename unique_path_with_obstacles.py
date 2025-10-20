c987dfa9a118d8d12ad01bae9be67d40700e9931
Chanakya Sinde <chanakyasinde@gmail.com>
2025-10-20 12:04:07 +0530

    Update unique_path_with_obstacles.py

--- unique_path_with_obstacles.py
+++ unique_path_with_obstacles.py
@@ -1,1 +1,23 @@
+def mazeObstacles(m, n, obstacleGrid):
+    dp = {}
+    mod = (10**9)+7
+    if obstacleGrid[0][0] == 1:
+        return 0
+    if obstacleGrid[m-1][n-1] == 1:
+        return 0
+    return solve(m-1,n-1, obstacleGrid, dp, mod)
 
+def solve(m, n, obstacleGrid, dp, mod):
+    if m == 0 and n == 0:
+        return 1
+    if m < 0 or n < 0:
+        return 0 
+    if obstacleGrid[m][n] == 1:
+        return 0
+
+    if (m,n) in dp:
+        return dp[(m,n)]
+
+    dp[(m,n)] = (solve(m-1, n,obstacleGrid, dp, mod) + solve(m, n-1,obstacleGrid, dp, mod)) % mod
+
+    return dp[(m, n)]
