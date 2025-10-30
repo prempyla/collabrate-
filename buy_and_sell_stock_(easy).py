fce1f56dd379d20b55e490ae26146e6668fb7b14
Chanakya Sinde <chanakyasinde@gmail.com>
2025-10-30 23:19:54 +0530

    Update buy_and_sell_stock_(easy).py

--- buy_and_sell_stock_(easy).py
+++ buy_and_sell_stock_(easy).py
@@ -1,1 +1,8 @@
-
+def maxProfit(prices):
+    n = len(prices)
+    mini = prices[0]
+    profit = 0
+    for i in range(1,n):
+        profit = max(profit, prices[i]-mini)
+        mini = min(prices[i], mini)
+    return profit
