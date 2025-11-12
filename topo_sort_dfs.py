1d7aa1bbeba037e84088e02f63465d75eee6e66b
Chanakya Sinde <chanakyasinde@gmail.com>
2025-11-12 23:16:02 +0530

    Update topo_sort_dfs.py

--- topo_sort_dfs.py
+++ topo_sort_dfs.py
@@ -1,1 +1,23 @@
+def topoSort(V, edges):
+    from collections import defaultdict
 
+    adj = defaultdict(list)
+    for u, v in edges:
+        adj[u].append(v)
+
+    visited = [False] * (V + 1)  
+    stack = []
+
+    def dfs(node):
+        visited[node] = True
+        for neighbor in adj[node]:
+            if not visited[neighbor]:
+                dfs(neighbor)
+        stack.append(node)  
+
+    for i in range(1, V + 1):
+        if not visited[i]:
+            dfs(i)
+
+    stack.reverse()
+    return stack
