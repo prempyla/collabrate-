674942669344ecfb79c06f7b655e71d98072b9cd
Chanakya Sinde <chanakyasinde@gmail.com>
2025-11-18 22:49:29 +0530

    Update rotting_oranges.py

--- rotting_oranges.py
+++ rotting_oranges.py
@@ -1,1 +1,40 @@
+from collections import deque
 
+def orangesRotting(grid):
+    q = deque()
+    n=len(grid)
+    m=len(grid[0])
+    time=0
+
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 2:
+                q.append( )
+    
+    while len(q)>0:
+        r,c,t = q.popleft()
+        
+        if t>time:
+            time=t
+
+        if r-1>=0 and grid[r-1][c]==1:
+            q.append([r-1,c,t+1])
+            grid[r-1][c]=2
+        
+        if r+1<n and grid[r+1][c]==1:
+            q.append([r+1,c,t+1])
+            grid[r+1][c]=2
+
+        if c-1>=0 and grid[r][c-1]==1:
+            q.append([r,c-1,t+1])
+            grid[r][c-1]=2
+
+        if c+1<m and grid[r][c+1]==1:
+            q.append([r,c+1,t+1])
+            grid[r][c+1]=2
+
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 1:
+                return -1
+    return time
