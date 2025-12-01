800fe175cc1c8a7784a72503606e45a05d3cb871
Chanakya Sinde <chanakyasinde@gmail.com>
2025-12-01 23:07:10 +0530

    Update find_largest_node_less_or_equals_to_k.py

--- find_largest_node_less_or_equals_to_k.py
+++ find_largest_node_less_or_equals_to_k.py
@@ -1,1 +1,16 @@
-
+'''
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+'''
+def findFloor(root, key):
+    result = None
+    while root:
+        if root.val > key:
+            root = root.left
+        else:
+            result = root
+            root = root.right
+    return result
