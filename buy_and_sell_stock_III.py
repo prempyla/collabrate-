e23f6180166d5b71d0747d6c5701891c52846bbf
Chanakya Sinde <chanakyasinde@gmail.com>
2025-12-03 23:28:34 +0530

    Update buy_and_sell_stock_III.py

--- buy_and_sell_stock_III.py
+++ buy_and_sell_stock_III.py
@@ -1,1 +1,18 @@
+def maxProfit(prices):
+    n = len(prices)
+    dp = {}
+    return solve(0, 0, 0, n, dp)
 
+def solve(i, buy, k, n, dp):
+    if i >= n:
+        return 0
+    if (i, buy, k) in dp:
+        return dp[(i, buy, k)]
+    if buy == 0 and k <= 2:
+        dp[(i, buy, k)] = max(solve(i+1, 1, k+1, n, dp) - prices[i], solve(i+1, 0, k, n, dp))
+    elif buy == 1 and k <= 2:
+        dp[(i, buy, k)] = max(solve(i+1, 0, k, n, dp) + prices[i], solve(i+1, 1, k, n, dp))
+    else:
+        return 0
+        
+    return dp[(i, buy, k)]
