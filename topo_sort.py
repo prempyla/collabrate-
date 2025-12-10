4bf1706dfe6e8165b8e2bb31b95a29c20212a63e
Chanakya Sinde <chanakyasinde@gmail.com>
2025-12-10 22:59:50 +0530

    Update topo_sort.py

--- topo_sort.py
+++ topo_sort.py
@@ -1,1 +1,24 @@
+def topoSort(V, edges):
+    adj = [[] for _ in range(V+1)]
+    visited = [False]*(V+1)
+    lst = []
 
+    for u, v in edges:
+        adj[u].append(v)
+
+    def dfs(node):
+        if visited[node] == True:
+            return 
+        visited[node] = True
+        
+        for neigh in adj[node]:
+            if not visited[neigh]:
+                dfs(neigh)
+        lst.append(node)
+
+
+    for i in range(1, V+1):
+        if visited[i] == False:
+            dfs(i)
+
+    return lst[::-1]
