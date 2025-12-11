05e2909a527a895277bd390eb8426b8c247640ea
Chanakya Sinde <chanakyasinde@gmail.com>
2025-12-11 22:19:52 +0530

    Update lcs_substring_.py

--- lcs_substring_.py
+++ lcs_substring_.py
@@ -1,1 +1,15 @@
-
+def longestCommonSubstring(s1, s2):
+    m, n = len(s1), len(s2)
+    
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    max_length = 0  
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                max_length = max(max_length, dp[i][j])
+            else:
+                dp[i][j] = 0  
+    
+    return max_length
