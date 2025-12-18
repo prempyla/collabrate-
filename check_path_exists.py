ac97e45b4dd589163a0dcf2a971ae82a72f65ef6
Chanakya Sinde <chanakyasinde@gmail.com>
2025-12-18 23:47:36 +0530

    Update check_path_exists.py

--- check_path_exists.py
+++ check_path_exists.py
@@ -1,1 +1,23 @@
+def isSibling(n, x, y, edges):
+    adj = [[] for _ in range(n)]
+    visited = [False]*n
+    lst = []
 
+    for u, v in edges:
+        adj[u].append(v)
+        adj[v].append(u)
+
+    def dfs(node):
+        if visited[node] == True:
+            return 
+        visited[node] = True
+        lst.append(node)
+
+        for neigh in adj[node]:
+            if not visited[neigh]:
+                dfs(neigh)
+
+    dfs(x)
+    if y in lst:
+        return True
+    return False
