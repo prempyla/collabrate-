5c9c22333b8b19a1797e6efd0cc126ea7665aacd
Chanakya Sinde <chanakyasinde@gmail.com>
2025-12-23 23:24:46 +0530

    Update sum_top_k_ele_min_heap.py

--- sum_top_k_ele_min_heap.py
+++ sum_top_k_ele_min_heap.py
@@ -2,7 +2,6 @@ import heapq
 
 class Solution:
     def sum_of_top_k_elements(self, heap, k):
-        # code here
         total = 0
         for i in range(k):
             total += heapq.heappop(heap)
