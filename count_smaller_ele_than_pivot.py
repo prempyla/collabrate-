1db0b278fb7f59ecd8cfcae5df5211cdee8c6d56
Chanakya Sinde <chanakyasinde@gmail.com>
2025-12-28 19:29:44 +0530

    Update count_smaller_ele_than_pivot.py

--- count_smaller_ele_than_pivot.py
+++ count_smaller_ele_than_pivot.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def countSmallerThanPivot(self, arr, pivot):
+        # code here
+        arr.sort()
+        ans = 0
+        for i in range(len(arr)):
+            if arr[i] < pivot:
+                ans += 1
+        return ans
