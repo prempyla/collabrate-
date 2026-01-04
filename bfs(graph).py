2d01eae6d962882019b9e95cf3e3bba9b3991629
Chanakya Sinde <chanakyasinde@gmail.com>
2026-01-04 19:54:19 +0530

    Update bfs(graph).py

--- bfs(graph).py
+++ bfs(graph).py
@@ -1,1 +1,25 @@
+from collections import deque
 
+def bfsOfGraph(n, edges, src):
+    visited = [False]*n
+    adj = [[] for _ in range(n)]
+    bfs = []
+
+    for u,v in edges:
+        adj[u].append(v)
+        adj[v].append(u)
+
+    q = deque()
+    q.append(src)
+    visited[src] = True
+
+    while q:
+        node = q.popleft()
+        bfs.append(node)
+
+        for neigh in adj[node]:
+            if not visited[neigh]:
+                q.append(neigh)
+                visited[neigh] = True
+
+    return bfs
