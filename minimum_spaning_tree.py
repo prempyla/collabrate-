4c4de48e8409152480d9a748fcc73b501ee7fd50
Chanakya Sinde <chanakyasinde@gmail.com>
2026-01-05 21:41:54 +0530

    Update minimum_spaning_tree.py

--- minimum_spaning_tree.py
+++ minimum_spaning_tree.py
@@ -1,1 +1,31 @@
+import heapq
 
+def minimumCost(N, edges):
+    adj = [[] for _ in range(N+1)]
+    visited = [False]*(N+1)
+
+    for v1, v2, cost in edges:
+        adj[v1].append((cost, v2))
+        adj[v2].append((cost, v1))
+
+    total_weight = 0
+    count = 0
+
+    q = [(0, 1)]
+
+    while q:
+        weight, node = heapq.heappop(q)
+        if visited[node] == True:
+            continue
+        visited[node] = True
+
+        total_weight += weight
+        count += 1
+
+        for wt, n in adj[node]:
+            if not visited[n]:
+                heapq.heappush(q, (wt, n))
+
+    if count != N:
+        return -1
+    return total_weight
