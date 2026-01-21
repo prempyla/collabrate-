0ebab5830d4ac3bdeec0e9a4b712390556f090a9
Chanakyasinde <chanakyasinde@gmail.com>
2026-01-21 13:00:00 +0530

    Add max subarray sum logic

---
+++ max_subarray_sum.py
@@ -0,0 +1,8 @@
+def max_subarray_sum(arr):
+    if not arr: return 0
+    max_so_far = arr[0]
+    curr_max = arr[0]
+    for i in range(1, len(arr)):
+        curr_max = max(arr[i], curr_max + arr[i])
+        max_so_far = max(max_so_far, curr_max)
+    return max_so_far
