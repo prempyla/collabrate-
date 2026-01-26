8011c7f98da0d56a2e0c51ee3962b0b6dc1e9989
Chanakya Sinde <chanakyasinde@gmail.com>
2026-01-26 21:06:42 +0530

    Update notifier_systems.ts

--- notifier_systems.ts
+++ notifier_systems.ts
@@ -1,1 +1,25 @@
+interface Notifier {
+  notify(message: string): void;
+}
 
+class EmailNotifier implements Notifier{
+    notify(message: string): void{
+        console.log(`[EMAIL] ${message}`)
+    }
+}
+
+class SMSNotifier implements Notifier{
+    notify(message: string): void{
+        console.log(`[SMS] ${message}`);
+    }
+}
+
+class PushNotifier implements Notifier{
+    notify(message: string): void{
+        console.log(`[PUSH] ${message}`);
+    }
+}
+
+function sendNotification(n: Notifier, msg: string) {
+    n.notify(msg);
+}
