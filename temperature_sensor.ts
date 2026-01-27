d3c3b1cf5393550dbbeb089ad2b905a6590fdaf0
Chanakya Sinde <chanakyasinde@gmail.com>
2026-01-27 21:58:32 +0530

    Update temperature_sensor.ts

--- temperature_sensor.ts
+++ temperature_sensor.ts
@@ -1,1 +1,21 @@
+class TemperatureSensor {
+  // write your code here
+  private temperature: number;
 
+  constructor(temperature: number) {
+    this.setTemperature(temperature);
+  }
+
+  readTemperature(): number{
+    return this.temperature;
+  }
+  setTemperature(temp: number): void{
+    if (temp < -50 || temp > 150) {
+        throw new Error("Temperature out of realistic range");
+    }
+    this.temperature = temp
+  }
+  increaseTemperature(delta: number): void{
+    this.setTemperature(this.temperature+delta)
+  }
+}
