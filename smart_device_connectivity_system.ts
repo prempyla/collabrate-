3f207d81f807132a201cb052150e588264678c99
Chanakya Sinde <chanakyasinde@gmail.com>
2026-02-07 00:26:35 +0530

    Update smart_device_connectivity_system.ts

--- smart_device_connectivity_system.ts
+++ smart_device_connectivity_system.ts
@@ -1,1 +1,43 @@
+interface Connectable {
+    connect(): void;
+    disconnect(): void;
+}
 
+abstract class SmartDevice {
+    id: string;
+
+    constructor(id: string) {
+        this.id = id;
+    }
+
+    info(): void {
+        console.log(`Device ID: ${this.id}`);
+    }
+    abstract performAction(): void;
+}
+
+// SmartLight class
+class SmartLight extends SmartDevice implements Connectable {
+    connect(): void {
+        console.log(`SmartLight connected`);
+    }
+    disconnect(): void {
+        console.log(`SmartLight disconnected`);
+    }
+    performAction(): void {
+        console.log(`Changing brightness`);
+    }
+}
+
+// SmartLock class
+class SmartLock extends SmartDevice implements Connectable {
+    connect(): void {
+        console.log("SmartLock connected");
+    }
+    disconnect(): void {
+        console.log("SmartLock disconnected");
+    }
+    performAction(): void {
+        console.log("Locking/Unlocking door");
+    }
+}
