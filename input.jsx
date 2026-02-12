cc0ae5b6f4a14364fa148357824077d5b6f3d0a3
Chanakya Sinde <chanakyasinde@gmail.com>
2026-02-12 20:23:40 +0530

    Update input.jsx

--- input.jsx
+++ input.jsx
@@ -1,1 +1,25 @@
+import React, { useState } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        placeholder="Type something..."
+        value={text}
+        onChange={handleChange}
+      />
+      
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default App;
 
