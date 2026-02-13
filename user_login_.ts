13a317a5c4db090add7d3886641b0e3834912275
Chanakya Sinde <chanakyasinde@gmail.com>
2026-02-13 21:13:29 +0530

    Update user_login_.ts

--- user_login_.ts
+++ user_login_.ts
@@ -1,1 +1,29 @@
+class UserSession {
+  isLoggedIn: boolean;
+  private duration: number;
 
+  constructor(isLoggedIn: boolean) {
+    this.isLoggedIn = isLoggedIn;
+    this.duration = 0;
+  }
+
+  startSession() {
+    if (this.isLoggedIn) {
+        this.duration += 1;
+    }
+  }
+
+  extendSession(minutes: number) {
+    if (minutes > 0) {
+      this.duration =+ minutes;
+    }
+  }
+
+  endSession() {
+    this.isLoggedIn = false;
+  }
+
+  getDuration(): number {
+    return this.duration;
+  }
+}
