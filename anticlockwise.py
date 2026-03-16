3806cec04e74405277aa01a8eeb4d9ade3d63b38
Chanakya Sinde <chanakyasinde@gmail.com>
2026-03-16 23:51:34 +0530

    Remove unused matrix initialization code

--- anticlockwise.py
+++ anticlockwise.py
@@ -3,10 +3,3 @@ mat=[]
 for _ in range(n):
     lst=list(map(int, input().split()))
     mat.append(lst)
-
-matrix=[]
-for i in range(m):
-    l=[]
-    for j in range(n):
-        l.append(0)
-    matrix.append(l)
