84e560e13fad3cf4023a217cc2970b2dddccddd0
Chanakya Sinde <chanakyasinde@gmail.com>
2026-04-05 22:40:46 +0530

    Clean up comments in shape detection function

--- shape_detection.py
+++ shape_detection.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
 def detect_distribution_shape(df: pd.DataFrame) -> str:
-    # Calculate skewness of PurchaseAmount
     skewness = df["PurchaseAmount"].skew()
     
-    # Determine shape
     if skewness > 0.5:
         return "Right Skewed"
     elif skewness < -0.5:
