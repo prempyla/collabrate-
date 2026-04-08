d6805544dc85e92036a33b33e279bd2f0eecd6ba
Chanakya Sinde <chanakyasinde@gmail.com>
2026-04-08 22:41:21 +0530

    Update outlier_detection_IQR.py

--- outlier_detection_IQR.py
+++ outlier_detection_IQR.py
@@ -1,1 +1,13 @@
+def count_salary_outliers(df):
+    # Write your code below
+    Q1 = df['Salary'].quantile(0.25)
+    Q3 = df['Salary'].quantile(0.75)
 
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - (1.5 * IQR)
+    upper_bound = Q3 + (1.5 * IQR)
+
+    outliers = df[(df['Salary'] < lower_bound) | (df['Salary'] > upper_bound)]
+    
+    return len(outliers)
