54b4a9c408f205385aab662fd7c38dbf4ca5e787
Chanakya Sinde <chanakyasinde@gmail.com>
2026-04-10 23:12:50 +0530

    Update purchase_outlier_detection.py

--- purchase_outlier_detection.py
+++ purchase_outlier_detection.py
@@ -1,1 +1,13 @@
-
+def calculate_outlier_proportion(df):
+    Q1 = df['PurchaseAmount'].quantile(0.25)
+    Q3 = df['PurchaseAmount'].quantile(0.75)
+    
+    IQR = Q3 - Q1
+    
+    upper_bound = Q3 + 1.5 * IQR
+    
+    outliers = df[df['PurchaseAmount'] > upper_bound]
+    
+    result = len(outliers) / len(df)
+    
+    return round(result, 2)
