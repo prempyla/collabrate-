ec4616efb41d3ddfc440fc1beae6019ca7c48b2e
Chanakya Sinde <chanakyasinde@gmail.com>
2026-04-12 22:43:46 +0530

    Update most_common_complaint.py

--- most_common_complaint.py
+++ most_common_complaint.py
@@ -1,1 +1,11 @@
+def most_common_complaint(df):
+    # Write your code below
+    counts = df['ComplaintType'].value_counts()
 
+    max_count = counts.max()
+    
+    top_complaints = counts[counts == max_count].index.tolist()
+    
+    for complaint in df['ComplaintType']:
+        if complaint in top_complaints:
+            return complaint
